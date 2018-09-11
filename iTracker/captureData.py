b2a3b36047b1b4fedb56460dec3e4780c1ab0211
Swapnil Pande <swapnil.pande98@gmail.com>
2018-09-11 15:05:41 -0500

    Added optional command line arguments for number of dots and time for each dot

--- iTracker/captureData.py
+++ iTracker/captureData.py
@@ -3,14 +3,15 @@ import argparse #Argument parsing
 #Retrieve command line options
 parser = argparse.ArgumentParser()
 parser.add_argument('image_directory', help = 'directory in which to save captured images')
-parser.add_argument('number_of_dots', help = 'number of dots to iterate over')
+parser.add_argument('-d', '--number-of-dots', help = 'number of dots to iterate over', default = '5')
+parser.add_argument('-t', '--time-per-dot', help = 'time to display each dot', default = '10')
 args = parser.parse_args()
 
 from utils.uiUtils import dataCollectionUI #Importing data collection UI object
 
-ui = dataCollectionUI(args.image_directory, 1, 0.5)
+ui = dataCollectionUI(args.image_directory, float(args.time_per_dot), 0.5)
 
 images = 0
-while images < args.number_of_dots:
+while images < int(args.number_of_dots):
     images = ui.updateUI()
 ui.writeData()
