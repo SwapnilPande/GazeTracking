c04110f358f54d3e792935f848c48a1a08ce39b0
Swapnil Pande <swapnil.pande98@gmail.com>
2018-09-11 14:58:43 -0500

    Added command line argument for number of points

--- iTracker/captureData.py
+++ iTracker/captureData.py
@@ -3,6 +3,7 @@ import argparse #Argument parsing
 #Retrieve command line options
 parser = argparse.ArgumentParser()
 parser.add_argument('image_directory', help = 'directory in which to save captured images')
+parser.add_argument('number_of_dots', help = 'number of dots to iterate over')
 args = parser.parse_args()
 
 from utils.uiUtils import dataCollectionUI #Importing data collection UI object
@@ -10,6 +11,6 @@ from utils.uiUtils import dataCollectionUI #Importing data collection UI object
 ui = dataCollectionUI(args.image_directory, 1, 0.5)
 
 images = 0
-while images < 2:
+while images < args.number_of_dots:
     images = ui.updateUI()
 ui.writeData()
